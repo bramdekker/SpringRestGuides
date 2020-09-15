46ed3eda5a00e57a3e07f0d5077cf3c09bbf1153
bramdekker <bram.dekker@student.uva.nl>
2020-09-15 22:47:43 +0200

    typo topics to topic

--- websocket-guide/src/main/java/com/example/websocketguide/GreetingController.java
+++ websocket-guide/src/main/java/com/example/websocketguide/GreetingController.java
@@ -9,7 +9,7 @@ import org.springframework.web.util.HtmlUtils;
 public class GreetingController {
     
     @MessageMapping("/hello")
-    @SendTo("/topics/greetings")
+    @SendTo("/topic/greetings")
     public Greeting greeting(HelloMessage message) throws Exception {
         Thread.sleep(1000);
         return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
